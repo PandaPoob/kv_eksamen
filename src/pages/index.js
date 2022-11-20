f71ae2fb1a24eacbd18ef9976bb430f702f452b2
Caroline Nielsen <caro112f@stud.kea.dk>
2022-11-20 20:05:17 +0100

    månedens katte initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,60 +6,56 @@ import url from "../api/url";
 import PageHead from "../components/layout/PageHead";
 
 
-function Home({data}) {
+function Home({sideData, katteData}) {
+
+  let maanedensKatte = katteData.filter(e => {
+    return e.acf.udvalgt === true;
+  })
+
+  console.log(maanedensKatte);
   
   return (
     <Box>
-      <PageHead {...data}/>
+      <PageHead {...sideData} />
       <Box position="relative" height={"90vh"} width={"100vw"} overflow="hidden">
-        <Image src={data.acf.splashbillede} alt="test-image" layout="fill" objectFit="cover" />
+        <Image src={sideData.acf.splashbillede} alt="test-image" layout="fill" objectFit="cover" />
         <Box position="absolute" top={"0"} left={["0", "0", "0", "50%", "50%"]} width={["100%", "100%", "100%", "50%", "50%"]} height={"100%"} bgColor="brand.darkGrey" opacity={"50%"}></Box>
         <Flex width={["100%", "100%", "100%", "50%", "50%"]} position="absolute" left={["0", "0", "0", "50%", "50%"]} top="30%" align={"center"} justify="center" direction={"column"} color="brand.white">
-          <Heading as={"h1"}>{data.acf.overskrift}</Heading>
-          <Text>{data.acf.undertekst}</Text>
+          <Heading as={"h1"}>{sideData.acf.overskrift}</Heading>
+          <Text>{sideData.acf.undertekst}</Text>
           <NextLink href={""} passHref legacyBehavior>
             <Link variant={"blueBtn"}>{""}</Link>
           </NextLink>
         </Flex>
       </Box>
-      <Heading as="h1" size="heading1">
-        heading1
-      </Heading>
-
-      <Heading as="h2" size="heading2">
-        heading2
-      </Heading>
-      <Heading as="h3" size="heading3">
-        heading3
-      </Heading>
-      <Heading as="h4" size="heading4">
-        heading4
-      </Heading>
-      <Text fontSize={"xxs"}>xxs tekst</Text>
-      <Text fontSize={"xs"}>xs tekst</Text>
-      <Text fontSize={"sm"}>small tekst</Text>
-      <Text fontSize={"md"}>medium tekst</Text>
-      <Text fontSize={"lg"}>large tekst</Text>
-      <Text fontSize={"xl"}>xlarge tekst</Text>
-      <Flex>
-        <NextLink href={`/`} passHref legacyBehavior>
-          <Link variant={"redBtn"}>Red button</Link>
-        </NextLink>
-        <NextLink href={`/`} passHref legacyBehavior>
-          <Link variant={"blueBtn"}>Blue button</Link>
-        </NextLink>
-      </Flex>
+      <Box>
+        <Heading as="h2" size={"heading2"}>
+          Månedens katte
+        </Heading>
+        <Text>Månedens katte er katte som har særligt udmærket sig i løbet af den forhenværende måned.</Text>
+        {maanedensKatte.map((e, i) => (
+          <Image key={i} src={e.acf.profil_billede} alt={e.acf.navn} width="400px" height={"240px"} />
+        ))}
+        {/* <Stack>
+          <Image />
+          <Box></Box>
+          <Image />
+        </Stack> */}
+      </Box>
     </Box>
   );
 }
 
 export async function getStaticProps() {
   const res = await fetch(`${url}indholdsider/165`);
-  const data = await res.json();
+  const sideData = await res.json();
+  const res2 = await fetch(`${url}adoptionskatte`);
+  const katteData = await res2.json();
 
   return {
     props: {
-      data: data,
+      sideData: sideData,
+      katteData: katteData,
     },
   };
 }
