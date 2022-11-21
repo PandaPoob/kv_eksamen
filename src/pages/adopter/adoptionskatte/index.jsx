ecb6f9b5fcd0f752e11f6b8dd39f8b917d7ffcf3
Freja <frej400d@stud.kea.dk>
2022-11-22 00:00:09 +0100

    cat data fix

--- src/pages/adopter/adoptionskatte/index.jsx
+++ src/pages/adopter/adoptionskatte/index.jsx
@@ -2,11 +2,12 @@ import url from "../../../api/url";
 import AdoptionsKatte from "../../../views/adopter/adoptionskatte/AdoptionsKatte";
 
 function AdoptionsKattePage({ data, sideData }) {
+  console.log(data);
   return <AdoptionsKatte data={data} sideData={sideData} />;
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${url}adoptionskatte`);
+  const res = await fetch(`${url}adoptionskatte?per_page`);
   const data = await res.json();
   const res2 = await fetch(`${url}indholdsider/166`);
   const sideData = await res2.json();
