6ca519d3f35761217d850c71aeaae794c1f953c1
Caroline Holm Nielsen <carolineholmnielsen@gmail.com>
2022-12-02 13:15:36 +0100

    internater done

--- src/views/internater/HjaelpTilSektion.jsx
+++ src/views/internater/HjaelpTilSektion.jsx
@@ -12,7 +12,7 @@ export default function HjaelpTilSektion({data}) {
     const cleanhjaelptilDataArr = hjaelptilDataArr.filter((t) => t.item !== false);
     
   return (
-    <Box as="section" bg={"brand.lightGrey"} display="grid" placeContent={"center"} py={{base:"3rem", lg: "6rem"}} px="1rem">
+    <Box id="blivfrivillig" as="section" bg={"brand.lightGrey"} display="grid" placeContent={"center"} py={{base:"3rem", lg: "6rem"}} px="1rem">
         <Box maxW={"container.lg"}>
             <Heading as="h2" size="heading2" textAlign={"center"}>Giv en hjælpende pote</Heading>
             <Box as="section" display="grid" gridTemplateColumns={{base: "1fr", md:"1fr 1fr"}} gap="3rem" alignItems={"start"}  py="3rem">
