a80e13b73279f4afa1305d2493a54c4f15d3ba3b
Caroline Nielsen <caro112f@stud.kea.dk>
2022-12-06 22:30:49 +0100

    wavy background

--- src/components/efterlyst+fremlyst/SearchBar.jsx
+++ src/components/efterlyst+fremlyst/SearchBar.jsx
@@ -3,7 +3,7 @@ import { Input } from '@chakra-ui/react'
 
 export default function SearchBar({searchInput, setSearchInput}) {
   return (
-    <Input maxW={{base: "200px", lg: "400px"}} fontSize="xs" placeholder='Søg efter by...' value={searchInput} onChange={(event) => setSearchInput(event.target.value)}/>
+    <Input w={{base: "230px", md: "400px", lg: "400px"}} fontSize="xs" placeholder='Søg på chipnr. eller øremærke...' value={searchInput} onChange={(event) => setSearchInput(event.target.value)}/>
     
   )
 }
--- src/components/layout/BackgroundWrapper.jsx
+++ src/components/layout/BackgroundWrapper.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Box } from '@chakra-ui/react'
-import bgimage from "../../../public/baggrund_stor.svg"
 
 export default function BackgroundWrapper({children}) {
   return (
-    <Box bgImage={bgimage} bgPos="center" bgSize={"cover"}>
+    <Box bgImage={"/baggrund_stor.svg"} bgPos="center" bgSize={"cover"} bgAttachment="fixed">
         {children}
     </Box>
   )
