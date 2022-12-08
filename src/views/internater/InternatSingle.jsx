5dfff342b9fba776a31d59a4d40b4e2394afb8c2
Freja <frej400d@stud.kea.dk>
2022-12-08 23:21:15 +0100

    small style fixes

--- src/views/internater/InternatSingle.jsx
+++ src/views/internater/InternatSingle.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import PageLayout from '../../components/layout/PageLayout'
-import KISplash from '../../components/klinikker+internater/KISplash'
-import Teamet from '../../components/klinikker+internater/Teamet'
-import LinkBanner from './LinkBanner';
-import KontaktSektion from './KontaktSektion';
-import HjaelpTilSektion from './HjaelpTilSektion';
-import OmInternatet from './OmInternatet';
+import React from "react";
+import PageLayout from "../../components/layout/PageLayout";
+import KISplash from "../../components/klinikker_internater/KISplash";
+import Teamet from "../../components/klinikker_internater/Teamet";
+import LinkBanner from "./LinkBanner";
+import KontaktSektion from "./KontaktSektion";
+import HjaelpTilSektion from "./HjaelpTilSektion";
+import OmInternatet from "./OmInternatet";
 
-export default function InternatSingle({internatData, MedarbejdData}) {
+export default function InternatSingle({ internatData, MedarbejdData }) {
   const acf = internatData.acf;
-  const by= acf.bynavn;
-  let teamet = MedarbejdData.filter((m)=>{
+  const by = acf.bynavn;
+  let teamet = MedarbejdData.filter((m) => {
     return m.acf.internat === by;
-})
+  });
   return (
     <PageLayout>
-        <KISplash acf={acf}/>
-        <LinkBanner data={acf.banner}/>
-        <OmInternatet data={acf.intro_sektion}/>
-        <HjaelpTilSektion data={acf.hjaelp_til_sektion}/>
-        <KontaktSektion data={acf.kontaktoplysninger}/>
-        <Teamet teamet={teamet}/>
-        
+      <KISplash acf={acf} />
+      <LinkBanner data={acf.banner} />
+      <OmInternatet data={acf.intro_sektion} />
+      <HjaelpTilSektion data={acf.hjaelp_til_sektion} />
+      <KontaktSektion data={acf.kontaktoplysninger} />
+      <Teamet teamet={teamet} />
     </PageLayout>
-  )
+  );
 }
--- src/views/katteklinikker/KatteklinikSingle.jsx
+++ src/views/katteklinikker/KatteklinikSingle.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import PageLayout from '../../components/layout/PageLayout'
-import { Box, Flex, Heading, Text } from '@chakra-ui/react'
-import KlinikBanner from './KlinikBanner';
-import KlinikIntroSektion from './KlinikIntroSektion';
-import KlinikKontaktinfo from './KlinikKontaktinfo';
-import Prisliste from './Prisliste';
-import Teamet from '../../components/klinikker+internater/Teamet';
-import HighlightSektion from './HighlightSektion';
-import KISplash from '../../components/klinikker+internater/KISplash';
+import React from "react";
+import PageLayout from "../../components/layout/PageLayout";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import KlinikBanner from "./KlinikBanner";
+import KlinikIntroSektion from "./KlinikIntroSektion";
+import KlinikKontaktinfo from "./KlinikKontaktinfo";
+import Prisliste from "./Prisliste";
+import Teamet from "../../components/klinikker_internater/Teamet";
+import HighlightSektion from "./HighlightSektion";
+import KISplash from "../../components/klinikker_internater/KISplash";
 
-export default function KatteklinikSingle({klinikData, MedarbejdData}) {
-    const acf = klinikData.acf;
-    const by= acf.bynavn;
-    let teamet = MedarbejdData.filter((m)=>{
-      return m.acf.katteklinik === by;
-  })
+export default function KatteklinikSingle({ klinikData, MedarbejdData }) {
+  const acf = klinikData.acf;
+  const by = acf.bynavn;
+  let teamet = MedarbejdData.filter((m) => {
+    return m.acf.katteklinik === by;
+  });
 
   return (
     <PageLayout>
-        <KISplash acf={acf}/>
-        <KlinikBanner acf={acf}/>
-        <KlinikIntroSektion  acf={acf}/>
-        <KlinikKontaktinfo acf={acf}/>
-        <Prisliste acf={acf} />
-        <Teamet teamet={teamet}/>
-        <HighlightSektion acf={acf}/>
+      <KISplash acf={acf} />
+      <KlinikBanner acf={acf} />
+      <KlinikIntroSektion acf={acf} />
+      <KlinikKontaktinfo acf={acf} />
+      <Prisliste acf={acf} />
+      <Teamet teamet={teamet} />
+      <HighlightSektion acf={acf} />
     </PageLayout>
-  )
+  );
 }
