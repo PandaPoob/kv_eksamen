92e57cd96afa8a3d626e68fc31ba0fc55d51918c
Freja <frej400d@stud.kea.dk>
2022-12-11 17:28:39 +0100

    kommuner animation

--- src/components/features/FindKommune.jsx
+++ src/components/features/FindKommune.jsx
@@ -20,6 +20,7 @@ import { useState, useEffect } from "react";
 import url from "../../api/url";
 import * as Yup from "yup";
 import { InputControl } from "formik-chakra-ui";
+import { motion } from "framer-motion";
 import NoResultsMsg from "../generics/NoResultsMsg";
 
 function FindKommune({ isOpen, onCloseComplete, onClose }) {
@@ -52,13 +53,13 @@ function FindKommune({ isOpen, onCloseComplete, onClose }) {
 
     if (kommune.length === 0) {
       setError(true);
+
+      setKommuneResult("");
     } else {
       setError(false);
       setKommuneResult(kommune);
     }
   }
-
-  console.log("here", kommuneResult);
 
   return (
     <Drawer
@@ -89,7 +90,7 @@ function FindKommune({ isOpen, onCloseComplete, onClose }) {
           />
         </DrawerHeader>
 
-        <DrawerBody px={{ base: "1rem", md: "4rem" }}>
+        <DrawerBody px={{ base: "1rem", md: "4rem" }} py="0">
           <Heading as="h1" size={"heading2"} mb="2rem" fontSize="xl">
             Søg efter din kommune
           </Heading>
@@ -98,6 +99,7 @@ function FindKommune({ isOpen, onCloseComplete, onClose }) {
             validationSchema={validationSchema}
             onSubmit={(values, actions) => {
               actions.setSubmitting(false);
+
               findKommune(values);
             }}
           >
@@ -129,7 +131,7 @@ function FindKommune({ isOpen, onCloseComplete, onClose }) {
                     borderStartRadius="0"
                     bg="brand.blueCta"
                     fontSize="xs"
-                    mt={{ base: "2.4rem", drawer: "0.8rem" }}
+                    mt={{ base: "3.8rem", drawer: "0.8rem" }}
                     colorScheme="teal"
                     _hover={{ transform: "scale(1)", bg: "brand.blue" }}
                     isLoading={props.isSubmitting}
@@ -143,87 +145,109 @@ function FindKommune({ isOpen, onCloseComplete, onClose }) {
           </Formik>
 
           {error ? (
-            <NoResultsMsg
-              msg={
-                "Vi kunne desværre ikke finde en kommune der matchede din søgning"
-              }
-            />
+            <Box
+              as={motion.div}
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              <NoResultsMsg
+                msg={
+                  "Vi kunne desværre ikke finde en kommune der matchede din søgning"
+                }
+              />
+            </Box>
           ) : null}
-          {kommuneResult !== "" ? (
-            kommuneResult[0].acf.kv_ikke_i_omrade ? (
-              <Box display="grid">
-                <Divider
-                  mb="2rem"
-                  orientation="horizontal"
-                  bg="brand.blue"
-                  borderColor="transparent"
-                />
-                <Heading as={"h2"} size="heading4" mb="1rem">
-                  <Text as="span" fontWeight="normal">
-                    {" "}
-                    Katten befinder sig på
-                  </Text>{" "}
-                  {kommuneResult[0].acf.kommune_navn}
-                </Heading>{" "}
-                <Text> {kommuneResult[0].acf.aftale_status}</Text>{" "}
+          {kommuneResult !== "" &&
+            (kommuneResult[0].acf.kv_ikke_i_omrade ? (
+              <Box
+                as={motion.div}
+                key={kommuneResult[0].id}
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5, ease: "easeOut" }}
+              >
+                <Box display="grid">
+                  <Divider
+                    mb="2rem"
+                    orientation="horizontal"
+                    bg="brand.blue"
+                    borderColor="transparent"
+                  />
+                  <Heading as={"h2"} size="heading4" mb="1rem">
+                    <Text as="span" fontWeight="normal">
+                      {" "}
+                      Katten befinder sig på
+                    </Text>{" "}
+                    {kommuneResult[0].acf.kommune_navn}
+                  </Heading>{" "}
+                  <Text> {kommuneResult[0].acf.aftale_status}</Text>{" "}
+                </Box>
               </Box>
             ) : (
-              <Box display="grid">
-                <Divider
-                  mb="2rem"
-                  orientation="horizontal"
-                  bg="brand.blue"
-                  borderColor="transparent"
-                />
-                <Heading as={"h2"} size="heading4" mb="0.5rem">
-                  <Text as="span" fontWeight="normal">
-                    Katten befinder sig i
-                  </Text>{" "}
-                  {kommuneResult[0].acf.kommune_navn}
-                </Heading>
-                <Flex alignItems="center" gap="0.5rem">
-                  {kommuneResult[0].acf.har_aftale ? (
-                    <>
-                      <Text fontWeight="bold">Kommunen betaler</Text>
-                      <Icon
-                        as={BsCheckCircle}
-                        h="1.5rem"
-                        w="1.5rem"
-                        color="green"
-                      />
-                    </>
-                  ) : (
-                    <>
-                      <Text fontWeight="bold">Kommunen betaler ikke</Text>{" "}
-                      <Icon
-                        as={BsXCircle}
-                        h="1.5rem"
-                        w="1.5rem"
-                        color="brand.errorMsg"
-                      />
-                    </>
+              <Box
+                as={motion.div}
+                key={kommuneResult[0].id}
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5, ease: "easeOut" }}
+              >
+                <Box display="grid">
+                  <Divider
+                    mb="2rem"
+                    orientation="horizontal"
+                    bg="brand.blue"
+                    borderColor="transparent"
+                  />
+                  <Heading as={"h2"} size="heading4" mb="0.5rem">
+                    <Text as="span" fontWeight="normal">
+                      Katten befinder sig i
+                    </Text>{" "}
+                    {kommuneResult[0].acf.kommune_navn}
+                  </Heading>
+                  <Flex alignItems="center" gap="0.5rem">
+                    {kommuneResult[0].acf.har_aftale ? (
+                      <>
+                        <Text fontWeight="bold">Kommunen betaler</Text>
+                        <Icon
+                          as={BsCheckCircle}
+                          h="1.5rem"
+                          w="1.5rem"
+                          color="green"
+                        />
+                      </>
+                    ) : (
+                      <>
+                        <Text fontWeight="bold">Kommunen betaler ikke</Text>{" "}
+                        <Icon
+                          as={BsXCircle}
+                          h="1.5rem"
+                          w="1.5rem"
+                          color="brand.errorMsg"
+                        />
+                      </>
+                    )}
+                  </Flex>
+                  <Text mt="2rem"> {kommuneResult[0].acf.aftale_status}</Text>
+                  <Text mt="1rem" mb="0.4rem">
+                    Kontakt:
+                  </Text>
+                  <Text>
+                    {kommuneResult[0].acf.kontaktnavn} <br />
+                    <Text as="span">{kommuneResult[0].acf.kontakt}</Text>
+                  </Text>
+                  {kommuneResult[0].acf.kontakt_kv && (
+                    <Box>
+                      <Text mt="1rem" mb="0.4rem">
+                        Tidsrum:
+                      </Text>
+                      <Text>{kommuneResult[0].acf.kvs_tlftid.tid_1}</Text>
+                      <Text>{kommuneResult[0].acf.kvs_tlftid.tid_2}</Text>
+                    </Box>
                   )}
-                </Flex>
-                <Text mt="2rem"> {kommuneResult[0].acf.aftale_status}</Text>
-                <Text mt="1rem" mb="0.4rem">
-                  Kontakt:
-                </Text>
-                <Text>
-                  {kommuneResult[0].acf.kontaktnavn} <br />
-                  <Text as="span">{kommuneResult[0].acf.kontakt}</Text>
-                </Text>
-                {kommuneResult[0].acf.kontakt_kv && (
-                  <Box>
-                    <Text mt="1rem" mb="0.4rem">
-                      Tidsrum:
-                    </Text>
-                    <Text>{kommuneResult[0].acf.kvs_tlftid.tid_1}</Text>
-                    <Text>{kommuneResult[0].acf.kvs_tlftid.tid_2}</Text>
-                  </Box>
-                )}
+                </Box>
               </Box>
-            )
-          ) : null}
+            ))}
         </DrawerBody>
 
         <DrawerFooter display={"grid"} justifyContent={"normal"}>
